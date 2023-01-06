cf4f4991ff194ec0aab0147024db6c613ada4437
jiiin✨ <jiiin.cho.dev@gmail.com>
2023-01-06 19:51:52 +0100

    Added storybook

--- packages/dalya-base/src/Button/Button.tsx
+++ packages/dalya-base/src/Button/Button.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import { ButtonWrapper } from './styles';
+import { ButtonWrapper, ButtonBase } from './styles';
 
 export interface ButtonProps {
   label: string;
-  logo?: string;
 }
 
 const Button = (props: ButtonProps) => {
   return (
     <ButtonWrapper>
-      <button>{props.label}</button>
-      <img src={props.logo} alt='logo' />
+      <ButtonBase>{props.label}</ButtonBase>
     </ButtonWrapper>
   );
 };
--- packages/dalya-base/src/Button/styles.ts
+++ packages/dalya-base/src/Button/styles.ts
@@ -1,8 +1,16 @@
 import styled from '@emotion/styled';
 
 export const ButtonWrapper = styled.div`
-  padding: 25px 150px;
+  padding: 8px;
   display: flex;
   justify-content: space-between;
   align-items: center;
+`;
+
+export const ButtonBase = styled.button`
+  padding: 8px;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
 `;
